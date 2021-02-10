e0b48bfd600e76417c08cfeb02e30be5394ae3f8
teekachu <tingbecker@gmail.com>
2021-02-10 10:29:20 -0800

    init

---
+++ DadJokes/DadJokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DadJokes
+//
+//  Created by Ting Becker on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
